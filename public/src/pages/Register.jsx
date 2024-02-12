a00c42ea06dcba8e62d68c134aec36e2475b3c0e
TSEPO PHETLA <tsepophetla@gmail.com>
2024-02-12 11:52:31 +0200

    new files

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Register() {
+  return (
+    <>
+    <FormContainer>
+<form onSubmit={(event)=>handleSubmit(event)}
+
+    </FormContainer>
+    </>
+  )
+}
+
+const FormContainer = styled.div``;
+
+export default Register;
